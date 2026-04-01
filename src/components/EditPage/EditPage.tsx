869963f257cd02875ca9a4f212080813c51aad2f
Lschanhi <lschanhi@outlook.com>
2026-03-31 22:28:28 -0300

    Adicionando outros inputs junto com um botao automático

--- src/components/EditPage/EditPage.tsx
+++ src/components/EditPage/EditPage.tsx
@@ -1,9 +1,23 @@
 import { useState } from "react"
 import { Input } from "./Input"
+import { Link } from "@tanstack/react-router";
+
+interface Link {
+    name: string,
+    url: string
+}
 
 export const EditPage = () => {
 
-   const [name, setName] = useState("Zezinho");
+    const [name, setName] = useState("Zezinho");
+    const [Links, setLinks] = useState<Link[]>([]);
+    const handleAddLink = () =>{setLinks([...Links,{name:"", url:""}])};
+    const handleLeLinkChange = (index: number, field: string, value:string)=>{
+        const updatesLinks = [...Links];
+        
+        updatesLinks[index] = {...updatesLinks[index],[field]: value,};
+        setLinks(updatesLinks);
+    };
 
 
     return (
@@ -12,22 +26,57 @@ export const EditPage = () => {
 
                 <main className="w-9/12 bg-red-800 h-screen p-4 ">
 
-                    <form className=" space-y-4">
-                        <Input name="Nome" id="name" placerholder="EX: Lucas" value={name} onChange={(event)=>{setName(event.target.value);}}/>
-                        <Input name="GitHub" id="github" placerholder="EX: Lschanhi" />
+                    <form className=" space-y-4" onSubmit={(event) => {event.preventDefault();}}>
+                        <Input name="Nome" id="name" placerholder="EX: Lucas" value={name} onChange={(event) => { setName(event.target.value); }} />
+                        <Input name="GitHub" id="github" placerholder="EX: Lschanhi" value={""} />
 
-                        <div className=" grid grid-cols-3 gap-4">
-                            <Input name="Linkedin" id="linkedin" placerholder="EX: Lucas/in" />
-                            <Input name="Instagram" id="instagram" placerholder="EX: @Lschanhi" />
-                            <Input name="TikTok" id="tiktok" placerholder="EX: Lucas" />
-                            <Input name="FaceBook" id="facebook" placerholder="EX: Lschanhi" />
-                            <Input name="Youtube" id="youtube" placerholder="EX: Lucas" />
+                        <div className=" grid grid-cols-2 gap-4">
+                            <Input name="Linkedin" id="linkedin" placerholder="EX: Lucas/in" value={""} />
+                            <Input name="Instagram" id="instagram" placerholder="EX: @Lschanhi" value={""} />
+                            <Input name="TikTok" id="tiktok" placerholder="EX: Lucas" value={""} />
+                            <Input name="FaceBook" id="facebook" placerholder="EX: Lschanhi" value={""} />
+                            <Input name="Youtube" id="youtube" placerholder="EX: Lucas" value={""} />
                         </div>
 
+                        <div className="space-y-4">
+                            {
+                                Links.map((link, index) => {
+                                    return (
+                                        <div className="flex grid grid-cols-2 gap-4">
+                                            <Input
+                                                name={"Link" + index + 1}
+                                                id={"link-" + index}
+                                                value={link.name}
+                                                placerholder="EX: Portifólio"
+                                                onChange={(event)=>{
+                                                    handleLeLinkChange(index, "name", event.target.value);
+                                                }}
+                                            />
 
+                                            <Input
+                                                name={"URL do Link"}
+                                                id={"link-url-" + index}
+                                                value={link.url}
+                                                placerholder="EX: https://meusite.com"
+                                                onChange={(event)=>{
+                                                    handleLeLinkChange(index, "url", event.target.value);
+                                                }}
+                                                />
+                                        </div>
+                                    );
+                                    
+                                })
+
+                                    
+                            }
+                                <button className="bg-white rounded-2xl cursor-pointer uppercase px-4 py-2
+                                 hover:opacity-90 transition-opacity" 
+                                 onClick={handleAddLink}>Adicionar Links</button>
+
+                        </div>
                     </form>
                 </main>
-                <aside className="w-3/12 bg-red-300 h-screen">
+                <aside className="w-4/12 bg-red-300 h-screen">
 
                     Seu Nome: {name}
                 </aside>
